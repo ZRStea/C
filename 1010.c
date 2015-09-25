03609ae6b682c23d5b9263fee52c7e2dffaef8a7
Wei Yulan <aheadlead@dlifep.com>
2015-09-25 19:30:24 +0800

    polish: coding style

--- 1010.c
+++ 1010.c
@@ -1,48 +1,62 @@
 #include <stdio.h>
 
-int prime(int num){
-    if (num==0||num==1)
+int prime(int num) 
+{
+    if (num == 0 || num == 1)
     {
        return 0;
     }
-    else{
-        for (int i = num - 1;i>=2;--i){
-            if (num%i == 0){
+    else
+    {
+        for (int i=num-1; i>=2; --i)
+        {
+            if (num%i == 0)
+            {
                 return 0;
             }
         }
-    return 1;
+        return 1;
     }
 }
 
-int maxn(char str[200]){
-    int maxnum= 0;
-    for (char n = 'a';(int)n<=122;++n){
+int maxn(char str[200]) 
+{
+    int maxnum=0;
+    for (char n='a'; (int)n<=122; ++n) 
+    {
         int tempnum=0;
-        for (int i = 0;str[i]!='\0';++i){
-            if (str[i]==n){
+        for (int i=0; str[i] != '\0'; ++i)
+        {
+            if (str[i] == n)
+            {
                 ++tempnum;
             }
         }
-        if (tempnum>maxnum){
+        if (tempnum > maxnum)
+        {
             maxnum = tempnum;
         }
-
     }
     return maxnum;
 }
 
-int minn(char str[200]){
-    int minnum= 0;
-    for (char n = 'a';(int)n<=122;++n){
+int minn(char str[200]) 
+{
+    int minnum=0;
+    for (char n='a'; (int)n<=122; ++n) 
+    {
         int tempnum=0;
-        for (int i = 0;str[i]!='\0';++i){
-            if (str[i]==n){
+        for (int i = 0; str[i]!='\0'; ++i)
+        {
+            if (str[i] == n)
+            {
                 ++tempnum;
             }
         }
-        if (minnum==0){
-            minnum = tempnum;}
+        if (minnum == 0)
+        {
+            minnum = tempnum;
+        }
         if (tempnum != 0)
         {
             if (tempnum < minnum)
@@ -55,18 +69,21 @@ int minn(char str[200]){
     return minnum;
 }
 
-int main(){
+int main()
+{
     char str[200];
-    scanf("%s",str);
+    scanf("%s", str);
     int mins = (maxn(str) - minn(str));
-    if (prime(mins)==1)
+    if (prime(mins) == 1)
     {
         printf("Lucky Word\n");
-        printf("%d\n",mins);
+        printf("%d\n", mins);
     }
-    else{
+    else
+    {
         printf("No Answer\n");
-        printf("%d\n",0);
+        printf("%d\n", 0);
     }
     return 0;
 }
+
