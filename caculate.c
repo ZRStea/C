f6847bf4cce922f2ff6f7ac98896d7436e338b34
ZRStea <zrstea@gmail.com>
2015-09-27 02:37:48 +0800

    完成了手动加法

--- caculate.c
+++ caculate.c
@@ -3,10 +3,12 @@
 char num1[2000];
 char num2[2000];
 char plus[2000];
-int length1;
-int length2;
 int pl()
 {
+    int length1;
+    int length2;
+    length2 = strlen(num2);
+    length1 = strlen(num1);
     if (length1>=length2)
     {    }
     else
@@ -17,11 +19,12 @@ int pl()
         length1 = t;
     }
     int temp,n = 0;
-    for (; length1>=0; )
+    for (; length1>=1; )
     {
-        if (length2 >= 0)
+        if (length2 >=1)
         {
             temp = (num1[length1-1] + num2[length2-1] + n) - 96;
+            // printf("%d\n", temp);
             if (temp < 10)
             {
                 plus[length1] = (char) (temp + 48);
@@ -51,20 +54,30 @@ int pl()
         --length1;
         }
     }
+    if (n == 1)
+    {
+        plus[0] = '1';
+    }
+    else
+    {
+        for(int i=0; i<=strlen(plus)+1; ++i)
+        {
+           plus[i] = plus[i+1]; 
+        }
+
+    }
     return 0;
 }
 
 int main()
 {
-    length2 = 4;
-    length1 = 5;
-    strcpy(num1,"12010");
-    strcpy(num2,"1203");
+    scanf("%s",num1);
+    scanf("%s",num2);
     pl();
     printf("%s\n",plus);
-    puts(num1);
-    puts(num2);
-    printf("%d\n",length2);
-    printf("%d\n",length1);
+    // puts(num1);
+    // puts(num2);
+    // printf("%d\n",length2);
+    // printf("%d\n",length1);
     return 0;
 }
