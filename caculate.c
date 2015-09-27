bbff59263a969afeb542228a8e1d6d8ff9af1feb
ZRStea <zrstea@gmail.com>
2015-09-28 01:08:14 +0800

    增加减法部分
    默认num1>num2 本来想做自动判断以及负数功能 听aheadlead的暂时搁置 可能有一些残余代码

--- caculate.c
+++ caculate.c
@@ -2,7 +2,7 @@
 #include <string.h>
 char num1[2000];
 char num2[2000];
-char plus[2000];
+char rlt[2000];
 int pl()
 {
     int length1;
@@ -31,13 +31,13 @@ int pl()
             // printf("%d\n", temp);
             if (temp < 10)
             {
-                plus[length1] = (char) (temp + 48);
+                rlt[length1] = (char) (temp + 48);
                 n = 0;
             }
             else
             {
                 n = 1;
-                plus[length1] = (char) (temp + 38);
+                rlt[length1] = (char) (temp + 38);
             }
         --length2;
         --length1;
@@ -47,28 +47,89 @@ int pl()
             temp = (num1[length1-1] + '0' + n) - 96;
             if (temp < 10)
             {
-                plus[length1] = (char) (temp + 48);
+                rlt[length1] = (char) (temp + 48);
                 n = 0;
             }
             else
             {
                 n = 1;
-                plus[length1] = (char) (temp + 38);
+                rlt[length1] = (char) (temp + 38);
             }
         --length1;
         }
     }
-    if (n == 1)
-    {
-        plus[0] = '1';
-    }
+    return 0;
+}
+
+int minus()
+{
+    int length1;
+    int length2;
+    length2 = strlen(num2);
+    length1 = strlen(num1);
+    if (length1>=length2)
+    {    }
     else
     {
-        for(int i=0; i<=strlen(plus)+1; ++i)
+        char numt[2000];
+        int t;
+        t = length2;
+        length2 = length1;
+        length1 = t;
+        strcpy(numt,num2);
+        strcpy(num2,num1);
+        strcpy(num1,numt);
+    }
+    int temp,n = 0;
+    for (; length1>=1; )
+    {
+        if (length2 >=1)
         {
-           plus[i] = plus[i+1]; 
+            temp = (num1[length1-1] - num2[length2-1] - n);
+            // printf("%d\n", temp);
+            if (temp >= 0)
+            {
+                rlt[length1] = (char) (temp + 48);
+                n = 0;
+            }
+            else
+            {
+                n = 1;
+                rlt[length1] = (char) (temp + 58);
+            }
+        --length2;
+        --length1;
         }
-
+        else
+        {
+            temp = (num1[length1-1] - '0' - n);
+            if (temp >= 0)
+            {
+                rlt[length1] = (char) (temp + 48);
+                n = 0;
+            }
+            else
+            {
+                n = 1;
+                rlt[length1] = (char) (temp + 58);
+            }
+        --length1;
+        }
+    }
+    for(int i=0; i<=strlen(rlt)+1; ++i)
+    {
+        rlt[i] = rlt[i+1]; 
+    }
+    for (; rlt[0] == '0'; )
+    {
+        for(int i=0; i<=strlen(rlt)+1; ++i)
+        {
+            rlt[i] = rlt[i+1]; 
+        }
+    }
+    if (rlt[0] == 0)
+    {
+        rlt[0] = '0';
     }
     return 0;
 }
@@ -77,8 +138,8 @@ int main()
 {
     scanf("%s",num1);
     scanf("%s",num2);
-    pl();
-    printf("%s\n",plus);
+    minus();
+    printf("%s\n",rlt);
     // puts(num1);
     // puts(num2);
     // printf("%d\n",length2);
