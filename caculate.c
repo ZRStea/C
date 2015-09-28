0007a9c54993c1eb23774bb3d0c7a60d17a3cc29
Wei Yulan <aheadlead@dlifep.com>
2015-09-28 09:12:01 +0800

    coding style polishing

--- caculate.c
+++ caculate.c
@@ -9,8 +9,9 @@ int pl()
     int length2;
     length2 = strlen(num2);
     length1 = strlen(num1);
-    if (length1>=length2)
-    {    }
+    if (length1 >= length2)
+    {
+    }
     else
     {
         char numt[2000];
@@ -18,44 +19,44 @@ int pl()
         t = length2;
         length2 = length1;
         length1 = t;
-        strcpy(numt,num2);
-        strcpy(num2,num1);
-        strcpy(num1,numt);
+        strcpy(numt, num2);
+        strcpy(num2, num1);
+        strcpy(num1, numt);
     }
-    int temp,n = 0;
+    int temp, n = 0;
     for (; length1>=1; )
     {
-        if (length2 >=1)
+        if (length2 >= 1)
         {
             temp = (num1[length1-1] + num2[length2-1] + n) - 96;
             // printf("%d\n", temp);
             if (temp < 10)
             {
-                rlt[length1] = (char) (temp + 48);
+                rlt[length1] = (char)(temp + 48);
                 n = 0;
             }
             else
             {
                 n = 1;
-                rlt[length1] = (char) (temp + 38);
+                rlt[length1] = (char)(temp + 38);
             }
-        --length2;
-        --length1;
+            --length2;
+            --length1;
         }
         else
         {
             temp = (num1[length1-1] + '0' + n) - 96;
             if (temp < 10)
             {
-                rlt[length1] = (char) (temp + 48);
+                rlt[length1] = (char)(temp + 48);
                 n = 0;
             }
             else
             {
                 n = 1;
-                rlt[length1] = (char) (temp + 38);
+                rlt[length1] = (char)(temp + 38);
             }
-        --length1;
+            --length1;
         }
     }
     return 0;
@@ -67,8 +68,9 @@ int minus()
     int length2;
     length2 = strlen(num2);
     length1 = strlen(num1);
-    if (length1>=length2)
-    {    }
+    if (length1 >= length2)
+    {
+    }
     else
     {
         char numt[2000];
@@ -76,44 +78,44 @@ int minus()
         t = length2;
         length2 = length1;
         length1 = t;
-        strcpy(numt,num2);
-        strcpy(num2,num1);
-        strcpy(num1,numt);
+        strcpy(numt, num2);
+        strcpy(num2, num1);
+        strcpy(num1, numt);
     }
     int temp,n = 0;
     for (; length1>=1; )
     {
-        if (length2 >=1)
+        if (length2 >= 1)
         {
             temp = (num1[length1-1] - num2[length2-1] - n);
             // printf("%d\n", temp);
             if (temp >= 0)
             {
-                rlt[length1] = (char) (temp + 48);
+                rlt[length1] = (char)(temp + 48);
                 n = 0;
             }
             else
             {
                 n = 1;
-                rlt[length1] = (char) (temp + 58);
+                rlt[length1] = (char)(temp + 58);
             }
-        --length2;
-        --length1;
+            --length2;
+            --length1;
         }
         else
         {
             temp = (num1[length1-1] - '0' - n);
             if (temp >= 0)
             {
-                rlt[length1] = (char) (temp + 48);
+                rlt[length1] = (char)(temp + 48);
                 n = 0;
             }
             else
             {
                 n = 1;
-                rlt[length1] = (char) (temp + 58);
+                rlt[length1] = (char)(temp + 58);
             }
-        --length1;
+            --length1;
         }
     }
     for(int i=0; i<=strlen(rlt)+1; ++i)
@@ -146,3 +148,4 @@ int main()
     // printf("%d\n",length1);
     return 0;
 }
+
